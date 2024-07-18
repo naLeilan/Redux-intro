720c432bc0e65fa4f0b68d3ce181ca9266fbef34
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-18 11:05:57 +0300

    redux file structure state slice

--- src/features/customers/Customer.jsx
+++ src/features/customers/Customer.jsx
@@ -1,10 +1,9 @@
 import { useSelector } from "react-redux";
-import store from "../../store";
 
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
 
-  return <h2>👋 Welcome, {customer} </h2>;
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
